62127cf2781e1307dcac05541991b647d8f5e4e4
Carlos Eduardo Rosar Kós Lassance <cadurosar@cadu-laptop>
2015-03-31 23:15:50 +0200

    Mudança no seletor pra pegar o atributo enabled

--- application/views/event/manage.php
+++ application/views/event/manage.php
@@ -1,27 +1,32 @@
 <script>$( document ).ready(function() {
   $("[name='my-checkbox']").bootstrapSwitch();
+  $('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
+    console.log(event); // jQuery event
+  });
 });
 </script>
 
 <div class="row">
-	<?php require_once APPPATH.'views/include/common_user_left_menu.php' ?>
+    <?php require_once APPPATH.'views/include/common_user_left_menu.php' ?>
 	<div class="col-lg-10 middle-content">
-    <a href="<?=$this->config->item("url_link")?>events/eventCreate"><button id="create" class="btn btn-default"  value="Criar novo evento" >Criar novo evento</button></a>
+        <a href="<?=$this->config->item("url_link")?>events/eventCreate"><button id="create" class="btn btn-default"  value="Criar novo evento" >Criar novo evento</button></a>
 
 		<?php 
 			if(isset($events) && count($events) > 0) {
 			    ?>
-               <div class="table middle-content"><table class="table"><tr><th>Nome</th><th>Data Inicio</th><th>Data Fim</th><th>Habilitar na Interface</th></tr> 
+               <table class="table"><tr><th>Nome</th><th>Data Inicio</th><th>Data Fim</th><th>Habilitar na Interface</th></tr> 
         <?php
             	foreach($events as $event) {
 		?><tr>
 		<td><a href="<?=$this->config->item("url_link")?>events/info/<?=$event->getEventId()?>"><?=$event->getEventName();?></a></td>
-        <td><?= date_format(date_create($event->getDateStart()), 'd/m/y H:i');?> </td>
-        <td><?= ($event->getDateStart() != $event->getDateFinish())? " - ".date_format(date_create($event->getDateFinish()), 'd/m/y H:i'):""?> </td>
-        <td><input type="checkbox" data-inverse="true" name="my-checkbox" data-size="mini" checked></td>
+        <td><?= date_format(date_create($event->getDateStart()), 'd/m/y');?> </td>
+        <td><?= date_format(date_create($event->getDateFinish()), 'd/m/y')?> </td>
+        <td><input type="checkbox" data-inverse="true" name="my-checkbox" data-size="mini" <?php if($event->isEnabled()) echo "checked";?></td>
 		</tr>
 		<?php 
 				}
+        ?> </table>
+        <?php
 			} else {
 		?>
 			<h3> 
