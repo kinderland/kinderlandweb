1057fb4c940be85c63a181475488824d1d013175
cadu <cadurosar@gmail.com>
2015-04-07 19:06:27 +0200

    Corrigir o tratamento de transações pro estilo do code igniter

--- application/models/generic_model.php
+++ application/models/generic_model.php
@@ -7,17 +7,20 @@
 
 		public function startTransaction(){
 			$this->Logger->info("[START TRANSACTION]");
-			$this->db->query("BEGIN TRANSACTION");
+//			$this->db->query("BEGIN TRANSACTION");
+			$this->db->trans_start();
 		}
 
 		public function commitTransaction(){
 			$this->Logger->info("[COMMIT TRANSACTION]");
-			$this->db->query("COMMIT TRANSACTION");
+//			$this->db->query("COMMIT TRANSACTION");
+			$this->db->trans_complete();
 		}
 
 		public function rollbackTransaction(){
 			$this->Logger->info("[ROLLBACK TRANSACTION]");
-			$this->db->query("ROLLBACK");
+//			$this->db->query("ROLLBACK");
+			$this->db->trans_rollback();
 		}
 	}
 ?>
