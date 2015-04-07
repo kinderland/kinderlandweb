e042c2fdbe67e8797b8db8ab6beeb7692a007c4d
cadu <cadurosar@gmail.com>
2015-04-07 18:46:22 +0200

    Criação da classe Donation

---
+++ application/core/donation.php
@@ -0,0 +1,86 @@
+<?php
+	class Donation {
+		public $donationId;
+		public $personId;
+		public $donationType;
+		public $dateCreated;
+		public $dateUpdated;
+		public $donatedValue;
+		public $donationStatus;
+
+		public function __construct($donationId, $personId, $donationType, $dateCreated, $dateUpdated, $donatedValue, $donationStatus){
+			$this->donationId = $donationId;
+			$this->personId = $personId;
+			$this->donationType = $donationType;
+			$this->dateCreated = $dateCreated;
+			$this->dateUpdated = $dateUpdated;
+			$this->donatedValue = $donatedValue;
+			$this->donationStatus = $donationStatus;
+			
+		}
+
+		public static function createDonationObject($resultRow){
+			return new Donation(
+				$resultRow->donation_id, 
+				$resultRow->person_id,
+				$resultRow->donation_type, 
+				$resultRow->date_created,
+				$resultRow->date_updated,
+				$resultRow->donated_value,
+				$resultRow->donation_status,
+				null
+			);
+		}
+		
+		public function setPersonId($personId){
+			$this->personId = $personId;
+		}
+		public function getPersonId(){
+			return $this->personId;
+		}
+
+		public function setDonationId($donationId){
+			$this->donationId = $donationId;
+		}
+		public function getDonationId(){
+			return $this->donationId;
+		}
+
+		public function setDateCreated($dateCreated){
+			$this->dateCreated = $dateCreated;
+		}
+		public function getDateCreated(){
+			return $this->dateCreated;
+		}
+
+		public function setDateUpdated($dateUpdated){
+			$this->dateUpdated = $dateUpdated;
+		}
+
+		public function getDateUpdated(){
+			return $this->dateUpdated;
+		}
+
+		public function setDonatedValue($donatedValue){
+			$this->donatedValue = $donatedValue;
+		}
+		public function getDonatedValue(){
+			return $this->donatedValue;
+		}
+
+		public function setDonationStatus($donationStatus){
+			$this->donationStatus = $donationStatus;
+		}
+		public function getDonationStatus(){
+			return $this->donationStatus;
+		}
+
+		public function setDonationType($donationType){
+			$this->donationType = $donationType;
+		}
+		public function getDonationType(){
+			return $this->donationType;
+		}
+
+	}
+?>
--- application/models/donation_model.php
+++ application/models/donation_model.php
@@ -1,83 +1,91 @@
 <?php
 require_once APPPATH . 'core/CK_Model.php';
 
-class donation_model extends CK_Model{
+class donation_model extends CK_Model {
 
-	public function __construct(){
+	public function __construct() {
 		parent::__construct();
 	}
-    
-    public function getDonationById($donationId){
-        $sql = "SELECT * FROM donation WHERE donation_id = ?";
-        $resultSet = $this->executeRow($this->db, $sql, array(intval($donationId)));
 
-        if($resultSet)
-            return $resultSet;//Donation::createDonationObject($row);
-            
-        return null;
-    }
+	public function getDonationById($donationId) {
+		$sql = "SELECT * FROM donation WHERE donation_id = ?";
+		$resultSet = $this -> executeRow($this -> db, $sql, array(intval($donationId)));
 
-    public function getDonationPortionsMax($donation){
-        $sql = "select * from payment_period where event_id in (select event_id from event_subscription where donation_id = ?) 
+		if ($resultSet)
+			return $resultSet;
+		Donation::createDonationObject($row);
+
+		return null;
+	}
+
+	public function getDonationPortionsMax($donation) {
+		$sql = "select * from payment_period where event_id in (select event_id from event_subscription where donation_id = ?) 
                 and date_start <= ? and date_finish >= ?";
-        $result = $this->executeRow($this->db, $sql, array($donation->donation_id, $donation->date_created, $donation->date_created));
+		$result = $this -> executeRow($this -> db, $sql, array($donation -> donation_id, $donation -> date_created, $donation -> date_created));
 
-        if($result)
-            return $result->portions;
-        else
-            return 1;
-    }
+		if ($result)
+			return $result -> portions;
+		else
+			return 1;
+	}
 
-    public function createDonation($userId, $totalPrice, $donationType){
-        $this->Logger->info("Running: ". __METHOD__);
-        $sql = "INSERT INTO donation(person_id, donated_value, donation_type, donation_status) 
+	public function createDonation($userId, $totalPrice, $donationType) {
+		$this -> Logger -> info("Running: " . __METHOD__);
+		$sql = "INSERT INTO donation(person_id, donated_value, donation_type, donation_status) 
                 VALUES ($userId, $totalPrice, $donationType, 1)";
-        $result = $this->executeReturningId($this->db, $sql);
+		$result = $this -> executeReturningId($this -> db, $sql);
 
-        if($result){
-            return $result;
-        }
-    }
+		if ($result) {
+			return $result;
+		}
+	}
 
-    public function updateDonationStatus($donationId, $donationStatus){
-        $this->Logger->info("Running: ". __METHOD__);
-        $sql = "UPDATE donation SET donation_status = ? WHERE donation_id = ?";
-        $result = $this->execute($this->db, $sql, array($donationStatus, intval($donationId)));
+	public function updateDonationStatus($donationId, $donationStatus) {
+		$this -> Logger -> info("Running: " . __METHOD__);
+		$sql = "UPDATE donation SET donation_status = ? WHERE donation_id = ?";
+		$result = $this -> execute($this -> db, $sql, array($donationStatus, intval($donationId)));
 
-        if($result)
-            return true;
-        
-        return false;
-    }
+		if ($result) {
+			$donation = getDonationById($donationId);
+			if ($donation.isForAssociation()) {
+				$this->sendMail($donation.getPersonId());
+				
+												// Cadastra sócio
+			}
+			return true;
 
-    public function userIsAlreadyAssociate($userId){
-        $this->Logger->info("Running: ". __METHOD__);
-        $sql = "SELECT * FROM associates WHERE person_id = ?";
-        $resultSet = $this->executeRow($this->db, $sql, array(intval($userId)));
+		}
 
-        if($resultSet)
-            return true;
-            
-        return false;
-    }
+		return false;
+	}
 
-    public function getDonationTypeMinimumPrice($donationType){
-        $this->Logger->info("Running: ". __METHOD__);
-        $sql = "SELECT minimum_price FROM donation_type WHERE donation_type = ?";
-        $resultSet = $this->executeRow($this->db, $sql, array($donationType));
+	public function userIsAlreadyAssociate($userId) {
+		$this -> Logger -> info("Running: " . __METHOD__);
+		$sql = "SELECT * FROM associates WHERE person_id = ?";
+		$resultSet = $this -> executeRow($this -> db, $sql, array(intval($userId)));
 
-        if($resultSet)
-            return $resultSet->minimum_price;
-            
-        return 0.00;
-    }
+		if ($resultSet)
+			return true;
 
-    public function getDonationsByUserId($userId){
-        $this->Logger->info("Running: ". __METHOD__);
-        $sql = "SELECT * FROM donation_detailed WHERE person_id = ? ORDER BY date_created DESC";
-        return $this->executeRows($this->db, $sql, array(intval($userId)));
-    }
+		return false;
+	}
 
-    
+	public function getDonationTypeMinimumPrice($donationType) {
+		$this -> Logger -> info("Running: " . __METHOD__);
+		$sql = "SELECT minimum_price FROM donation_type WHERE donation_type = ?";
+		$resultSet = $this -> executeRow($this -> db, $sql, array($donationType));
+
+		if ($resultSet)
+			return $resultSet -> minimum_price;
+
+		return 0.00;
+	}
+
+	public function getDonationsByUserId($userId) {
+		$this -> Logger -> info("Running: " . __METHOD__);
+		$sql = "SELECT * FROM donation_detailed WHERE person_id = ? ORDER BY date_created DESC";
+		return $this -> executeRows($this -> db, $sql, array(intval($userId)));
+	}
+
 }
 ?>
