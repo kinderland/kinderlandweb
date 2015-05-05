be76ac5dc822f2f644890fda53e353512e42c007
Ruben Zonenschein <ruben@digitok.com.br>
2015-05-05 20:00:27 -0300

    Adicionando link para relatorio de doações

--- application/views/include/director_left_menu.php
+++ application/views/include/director_left_menu.php
@@ -3,9 +3,9 @@
         <li>Relatórios
             <ul class="nav nav-pills nav-stacked">
                 <li><a href="<?= $this->config->item('url_link'); ?>reports/user_registered">Usuários</a></li>
-                <!-- Relatorio ainda nao funcionando, descomentar quando estiver operante
+                <!-- Relatorio ainda nao funcionando, descomentar quando estiver operante -->
                 	<li><a href="<?= $this->config->item('url_link'); ?>reports/payments_bycard">Pagamentos</a></li>
-                -->
+                
             </ul>
         </li>
 </div>
