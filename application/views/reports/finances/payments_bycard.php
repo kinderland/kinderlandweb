f6e4e3e55def22fe62249faa12ef2e6d39fc0b57
Patrick Sava <patrick.sava@gmail.com>
2015-05-18 16:02:22 -0300

    Design das tabelas de relatório

--- application/views/reports/finances/payments_bycard.php
+++ application/views/reports/finances/payments_bycard.php
@@ -1,3 +1,25 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Colônia Kinderland</title>
+
+        <link href="<?= $this->config->item('assets'); ?>css/basic.css" rel="stylesheet" />
+        <!--<link href="<?= $this->config->item('assets'); ?>css/old/screen.css" rel="stylesheet" />-->
+        <link href="<?= $this->config->item('assets'); ?>css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/themes/base/jquery-ui.css" />
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/bootstrap-switch.min.css"></script>
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/theme.default.css" />
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/ui/jquery-ui.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquerysettings.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.redirect.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/formValidationFunctions.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap-switch.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.mask.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery.tablesorter.js"></script>
+
+    </head>
 <?php
 function imprimeDados($result, $tipo, $cartao) {
 	$total = 0;
@@ -17,118 +39,103 @@ function imprimeDados($result, $tipo, $cartao) {
 
 }
 ?>
+<body>
+	<div class = "row">
+		<div class="col-lg-10" bgcolor="red">
+			<h4>Número de sócios contribuintes: <?php echo $associates ?> </h4>
+			<h4>Número de doações avulsas: <?php echo $avulsas ?> </h4>
 
-<div class = "row">
-	<div class="col-lg-10" bgcolor="red">
-		<h5>Número de sócios contribuintes: <?php echo $associates ?> </h5>
-		<h5>Número de doações avulsas: <?php echo $avulsas ?> </h5>
-
-		<link href="http://www.kinderlandteste.com/kinderlandweb/assets/css/basic.css" rel="stylesheet" />
-		<!--<link href="http://www.kinderlandteste.com/kinderlandweb/assets/css/old/screen.css" rel="stylesheet" />-->
-		<link href="http://www.kinderlandteste.com/kinderlandweb/assets/css/bootstrap.min.css" rel="stylesheet" />
-		<link rel="stylesheet" href="http://www.kinderlandteste.com/kinderlandweb/assets/css/themes/base/jquery-ui.css" />
-		<link rel="stylesheet" href="http://www.kinderlandteste.com/kinderlandweb/assets/css/bootstrap-switch.min.css">
-		</script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquery-2.0.3.min.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/ui/jquery-ui.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquerysettings.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquery/jquery.redirect.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/formValidationFunctions.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/bootstrap-switch.min.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquery/jquery.mask.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquery.tablesorter.js"></script>
-		<script type="text/javascript" src="http://www.kinderlandteste.com/kinderlandweb/assets/js/jquery.tablesorter.widgets.js"></script>
-
-<!--		
-		<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard">
-		<button class="btn btn-primary" style="margin: 0px auto; ">Todos os pagamentos</button>
-		</a>
-		<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard?type=captured">
-		<button class="btn btn-primary" style="margin: 0px auto; ">Pagamentos finalizados</button>
-		</a>
-		<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard?type=canceled">
-		<button class="btn btn-primary" style="margin: 0px auto; ">Pagamentos cancelados</button>
-		</a>
-		<br>
--->	
-		<table class="table table-bordered table-striped" style="max-width: 850px;">
-			<tr>
-				<td colspan="8"> <h4> <b>Cartão de crédito: </b></h4> </td> <?php $tipo = "credito"; ?>
-			</tr>
-			<tr>
-				<td style="text-align: right;"><h4> <b> Bandeira do cartão </b></h4> </td>
-				<td style="text-align: center;"><h4> <b> Total </b></h4> </td>
-				<td><h4> <b> 1x </b></h4></td>
-				<td><h4> <b> 2x </b></h4></td>
-				<td><h4> <b> 3x </b></h4></td>
-				<td><h4> <b> 4x </b></h4></td>
-				<td><h4> <b> 5x </b></h4></td>
-				<td><h4> <b> 6x </b></h4> </td>
-			</tr>
-			<tr>
-				<td style="text-align: right;"> Amex </td>
-				<?php $cartao = "amex";
-					imprimeDados($result, $tipo, $cartao);
-				?>
-			</tr>
-			<tr>
-				<td style="text-align: right;"> Mastercard </td>
-				<?php $cartao = "mastercard";
-					imprimeDados($result, $tipo, $cartao);
-				?>
-			</tr>
-			<tr>
-				<td style="text-align: right;"> Visa </td>
-				<?php $cartao = "visa";
-					imprimeDados($result, $tipo, $cartao);
-				?>
-			</tr>
-			<tr>
-				<td style="text-align: right;"> Totais crédito </td>
-				<td style="text-align: center;"><?php $total = 0;
-					foreach ($credito as $resultado)
-						$total += $resultado;
-					echo $total;
-				?>
-				<td><?php echo $credito[1]?></td>
-				<td><?php echo $credito[2]?></td>
-				<td><?php echo $credito[3]?></td>
-				<td><?php echo $credito[4]?></td>
-				<td><?php echo $credito[5]?></td>
-				<td><?php echo $credito[6]?></td>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2" style="text-align: center;"> <h4> <b>Cartão de débito: </b></h4> </td>
-			</tr>
-			<tr>
+	<!--		
+			<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard">
+			<button class="btn btn-primary" style="margin: 0px auto; ">Todos os pagamentos</button>
+			</a>
+			<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard?type=captured">
+			<button class="btn btn-primary" style="margin: 0px auto; ">Pagamentos finalizados</button>
+			</a>
+			<a href="<?= $this -> config -> item('url_link'); ?>reports/payments_bycard?type=canceled">
+			<button class="btn btn-primary" style="margin: 0px auto; ">Pagamentos cancelados</button>
+			</a>
+			<br>
+	-->	
+			<table class="table table-bordered table-striped table-min-td-size" style="max-width: 600px;">
+				<tr>
+					<td colspan="8"> <h4> <b>Cartão de crédito: </b></h4> </td> <?php $tipo = "credito"; ?>
+				</tr>
+				<tr>
 					<td style="text-align: right;"><h4> <b> Bandeira do cartão </b></h4> </td>
-				<td style="text-align: center;"><h4> <b> Total </b></h4> </td>
+					<td style="text-align: center;"><h4> <b> Total </b></h4> </td>
+					<td><h4> <b> 1x </b></h4></td>
+					<td><h4> <b> 2x </b></h4></td>
+					<td><h4> <b> 3x </b></h4></td>
+					<td><h4> <b> 4x </b></h4></td>
+					<td><h4> <b> 5x </b></h4></td>
+					<td><h4> <b> 6x </b></h4></td>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Amex </td>
+					<?php $cartao = "amex";
+						imprimeDados($result, $tipo, $cartao);
+					?>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Mastercard </td>
+					<?php $cartao = "mastercard";
+						imprimeDados($result, $tipo, $cartao);
+					?>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Visa </td>
+					<?php $cartao = "visa";
+						imprimeDados($result, $tipo, $cartao);
+					?>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Totais crédito </td>
+					<td style="text-align: center;"><?php $total = 0;
+						foreach ($credito as $resultado)
+							$total += $resultado;
+						echo $total;
+					?>
+					<td><?php echo $credito[1]?></td>
+					<td><?php echo $credito[2]?></td>
+					<td><?php echo $credito[3]?></td>
+					<td><?php echo $credito[4]?></td>
+					<td><?php echo $credito[5]?></td>
+					<td><?php echo $credito[6]?></td>
+				</tr>
+			</table>
+			<table class="table table-bordered table-striped table-min-td-size" style="max-width: 600px;">
+				<tr>
+					<td colspan="2" style="text-align: center;"> <h4> <b>Cartão de débito: </b></h4> </td>
+				</tr>
+				<tr>
+					<td style="text-align: right;"><h4> <b> Bandeira do cartão </b></h4> </td>
+					<td style="text-align: center;"><h4> <b> Total </b></h4> </td>
 
-			</tr>
-			<tr>
-				<td colspan=1 style="text-align: right;"> Maestro </td>
-				<td colspan=1 style="text-align: center;"><?php
-				if (isset($result["debito"]["mastercard"][1]))
-					echo $result["debito"]["mastercard"][1];
-				else
-					echo 0;
- ?></td>
-			</tr>
-			<tr>
-				<td colspan=1 style="text-align: right;"> Visa Electron </td>
-				<td colspan=1 style="text-align: center;"><?php
-				if (isset($result["debito"]["visa"][1]))
-					echo $result["debito"]["visa"][1];
-				else
-					echo 0;
-				?></td>
-			</tr>
-			<tr>
-				<td colspan=1 style="text-align: right;"> Total débito </td>
-				<td colspan=1 style="text-align: center;"><?php echo $debito; ?></td>
-			</tr>
-		</table>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Maestro </td>
+					<td style="text-align: center;"><?php
+						if (isset($result["debito"]["mastercard"][1]))
+							echo $result["debito"]["mastercard"][1];
+						else
+							echo 0;
+		 			?></td>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Visa Electron </td>
+					<td style="text-align: center;"><?php
+					if (isset($result["debito"]["visa"][1]))
+						echo $result["debito"]["visa"][1];
+					else
+						echo 0;
+					?></td>
+				</tr>
+				<tr>
+					<td style="text-align: right;"> Total débito </td>
+					<td style="text-align: center;"><?php echo $debito; ?></td>
+				</tr>
+			</table>
+		</div>
 	</div>
-</div>
+</body>
