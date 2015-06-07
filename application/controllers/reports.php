fc1f36810b892827c4965e34dff1136782bea781
cadu <cadurosar@gmail.com>
2015-06-07 18:24:59 +0200

    Colocar geração de csv como especificado na tarefa #821

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -4,163 +4,178 @@ require_once APPPATH . 'core/CK_Controller.php';
 
 class Reports extends CK_Controller {
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->model('personuser_model');
-        $this->load->model('cielotransaction_model');
-        $this->load->model('donation_model');
-        $this->load->model('campaign_model');
-        $this->personuser_model->setLogger($this->Logger);
-        $this->cielotransaction_model->setLogger($this->Logger);
-        $this->donation_model->setLogger($this->Logger);
-        $this->campaign_model->setLogger($this->Logger);
-    }
+	public function __construct() {
+		parent::__construct();
+		$this -> load -> helper('url');
+		$this -> load -> model('personuser_model');
+		$this -> load -> model('cielotransaction_model');
+		$this -> load -> model('donation_model');
+		$this -> load -> model('campaign_model');
+		$this -> personuser_model -> setLogger($this -> Logger);
+		$this -> cielotransaction_model -> setLogger($this -> Logger);
+		$this -> donation_model -> setLogger($this -> Logger);
+		$this -> campaign_model -> setLogger($this -> Logger);
+	}
 
-    public function user_reports() {
-        $this->loadView("reports/users/user_reports_container");
-    }
+	public function user_reports() {
+		$this -> loadView("reports/users/user_reports_container");
+	}
 
-    public function finance_reports() {
-        $this->loadView("reports/finances/finance_reports_container");
-    }
+	public function finance_reports() {
+		$this -> loadView("reports/finances/finance_reports_container");
+	}
 
-    public function user_registered() {
-        $data['users'] = $this->personuser_model->getAllUserRegistered();
-        $this->loadReportView("reports/users/user_registered", $data);
-    }
+	public function user_registered() {
+		$data['users'] = $this -> personuser_model -> getAllUserRegistered();
+		$this -> loadReportView("reports/users/user_registered", $data);
+	}
 
-    public function all_users() {
-        $data['users'] = $this->personuser_model->getAllUsersDetailed();
-        $this->loadReportView("reports/users/all_users", $data);
-    }
+	public function all_users() {
+		$data['users'] = $this -> personuser_model -> getAllUsersDetailed();
+		$this -> loadReportView("reports/users/all_users", $data);
+	}
 
-    public function payments_bycard() {
-        $type = $this->input->get('type', TRUE);
-        $option = $this->input->get('option', TRUE);
-        //Por enquanto só o tipo finalizado é pra ser mostrado.
-        $type = "captured";
-        $title_extra = "";
-        $searchfor = FALSE;
-        if ($type == "captured") {
-            $searchfor = 6;
-            $title_extra = " - Finalizados";
-        } else if ($type == "canceled") {
-            $searchfor = 9;
-            $title_extra = " - Cancelados";
-        }
-        $results = $this->cielotransaction_model->statisticsPaymentsByCardFlag($searchfor,$option);
-        $data['result'] = $results;
-		if($option == PAYMENT_REPORTBYCARD_VALUES){
-	        $data['avulsas'] = $this->donation_model->sumFreeDonations();
-	        $sumAssociates = $this->donation_model->sumPayingAssociates();
-			$data['associates'] = $sumAssociates;				
-		}else {
-	        $data['avulsas'] = $this->donation_model->countFreeDonations();
-	        $countAssociates = $this->donation_model->countPayingAssociates();
-			$data['associates'] = $countAssociates;	
+	public function toCSV() {
+		$data = $this -> input -> post('data', TRUE);
+		$name = $this -> input -> post('name', TRUE);
+		$columnNames = $this -> input -> post('columName', TRUE);
+		$dataArray = json_decode($data);
+		if($columnNames)
+			$columnNamesArray = json_decode($columnNames);
+		else
+			$columnNamesArray = array();
+		try {
+			arrayToCSV($this -> Logger, $name, $dataArray,$columnNamesArray);
+		} catch(Exception $e) {
+			echo "<script>alert('Problema ao gerar csv, tente novamente mais tarde');</script>";
 		}
-        $creditos[1] = 0;
-        $creditos[2] = 0;
-        $creditos[3] = 0;
-        $creditos[4] = 0;
-        $creditos[5] = 0;
-        $creditos[6] = 0;
-        foreach ($results["credito"] as $credito) {
-            for ($i = 1; $i <= 6; $i++)
-                if (isset($credito[$i]))
-                    $creditos[$i] += $credito[$i];
-        }
-        $debito = 0;
-        if (isset($results["debito"])) {
-            foreach ($results["debito"] as $result) {
-                foreach ($result as $valor)
-                    $debito += $valor;
-            }
-        }
-        $data['credito'] = $creditos;
-        $data['debito'] = $debito;
-        $data['title_extra'] = $title_extra;
+	}
+
+	public function payments_bycard() {
+		$type = $this -> input -> get('type', TRUE);
+		$option = $this -> input -> get('option', TRUE);
+		//Por enquanto só o tipo finalizado é pra ser mostrado.
+		$type = "captured";
+		$title_extra = "";
+		$searchfor = FALSE;
+		if ($type == "captured") {
+			$searchfor = 6;
+			$title_extra = " - Finalizados";
+		} else if ($type == "canceled") {
+			$searchfor = 9;
+			$title_extra = " - Cancelados";
+		}
+		$results = $this -> cielotransaction_model -> statisticsPaymentsByCardFlag($searchfor, $option);
+		$data['result'] = $results;
+		if ($option == PAYMENT_REPORTBYCARD_VALUES) {
+			$data['avulsas'] = $this -> donation_model -> sumFreeDonations();
+			$sumAssociates = $this -> donation_model -> sumPayingAssociates();
+			$data['associates'] = $sumAssociates;
+		} else {
+			$data['avulsas'] = $this -> donation_model -> countFreeDonations();
+			$countAssociates = $this -> donation_model -> countPayingAssociates();
+			$data['associates'] = $countAssociates;
+		}
+		$creditos[1] = 0;
+		$creditos[2] = 0;
+		$creditos[3] = 0;
+		$creditos[4] = 0;
+		$creditos[5] = 0;
+		$creditos[6] = 0;
+		foreach ($results["credito"] as $credito) {
+			for ($i = 1; $i <= 6; $i++)
+				if (isset($credito[$i]))
+					$creditos[$i] += $credito[$i];
+		}
+		$debito = 0;
+		if (isset($results["debito"])) {
+			foreach ($results["debito"] as $result) {
+				foreach ($result as $valor)
+					$debito += $valor;
+			}
+		}
+		$data['credito'] = $creditos;
+		$data['debito'] = $debito;
+		$data['title_extra'] = $title_extra;
 		$data['option'] = $option;
-        $this->loadReportView("reports/finances/payments_bycard", $data);
-    }
+		$this -> loadReportView("reports/finances/payments_bycard", $data);
+	}
 
-    public function associated_campaign() {
-        $data['years'] = $this->campaign_model->getYearsCampaign();
-        $this->loadView("reports/associated/associated_campaign", $data);
-    }
+	public function associated_campaign() {
+		$data['years'] = $this -> campaign_model -> getYearsCampaign();
+		$this -> loadView("reports/associated/associated_campaign", $data);
+	}
 
-    public function associated_year($year) {
-        $data['summary'] = $this->campaign_model->getAssociatedCount($year);
-        $data['users'] = $this->personuser_model->getAllContribuintsDetailed();
-        $this->loadReportView("reports/associated/associated_year", $data);
-    }
-	
+	public function associated_year($year) {
+		$data['summary'] = $this -> campaign_model -> getAssociatedCount($year);
+		$data['users'] = $this -> personuser_model -> getAllContribuintsDetailed();
+		$this -> loadReportView("reports/associated/associated_year", $data);
+	}
+
 	public function all_transactions() {
-        $this -> Logger -> info("Starting " . __METHOD__);
-		
+		$this -> Logger -> info("Starting " . __METHOD__);
+
 		if (!$this -> checkPermition(array(SYSTEM_ADMIN))) {
-            $this -> denyAcess(___METHOD___);
-        }
-		
-        $ano = $this->input->get('ano', TRUE);
-        $data['payments'] = $this->cielotransaction_model->getPaymentsDetailed($ano);
-		$data['years'] = $this->cielotransaction_model->getPaymentYears();
-		if($ano)
+			$this -> denyAcess(___METHOD___);
+		}
+
+		$ano = $this -> input -> get('ano', TRUE);
+		$data['payments'] = $this -> cielotransaction_model -> getPaymentsDetailed($ano);
+		$data['years'] = $this -> cielotransaction_model -> getPaymentYears();
+		if ($ano)
 			$data['ano'] = $ano;
-        $this->loadReportView("reports/finances/all_transactions", $data);
-    }
+		$this -> loadReportView("reports/finances/all_transactions", $data);
+	}
 
-    public function user_donation_history() {
-        $data['users'] = $this->personuser_model->getAllUsersDetailed();
-        $this->loadReportView("reports/finances/donation_history", $data);
-    }
+	public function user_donation_history() {
+		$data['users'] = $this -> personuser_model -> getAllUsersDetailed();
+		$this -> loadReportView("reports/finances/donation_history", $data);
+	}
 
-    public function associate_campaign_donations() {
-        $years = array();
-        $start = 2015;
-        $end = date('Y');
-        while ( $start <= $end ){
-            $years[] = $start;
-            $start++;
-        }
+	public function associate_campaign_donations() {
+		$years = array();
+		$start = 2015;
+		$end = date('Y');
+		while ($start <= $end) {
+			$years[] = $start;
+			$start++;
+		}
 
-        $month = null;
-        $year = null;
-        if(isset($_GET['mes']) && $_GET['mes'] != 0)
-            $month = $_GET['mes'];
-        if(isset($_GET['ano']) && $_GET['ano'] != 0)
-            $year = $_GET['ano'];
+		$month = null;
+		$year = null;
+		if (isset($_GET['mes']) && $_GET['mes'] != 0)
+			$month = $_GET['mes'];
+		if (isset($_GET['ano']) && $_GET['ano'] != 0)
+			$year = $_GET['ano'];
 
-        $data['year'] = $year;
-        $data['years'] = $years;
-        $data['month'] = $month;
-        $data['donations'] = $this->donation_model->getDonationsDetailed(DONATION_TYPE_ASSOCIATE, $month, $year);
-        $this->loadReportView("reports/finances/donations", $data);
-    }
+		$data['year'] = $year;
+		$data['years'] = $years;
+		$data['month'] = $month;
+		$data['donations'] = $this -> donation_model -> getDonationsDetailed(DONATION_TYPE_ASSOCIATE, $month, $year);
+		$this -> loadReportView("reports/finances/donations", $data);
+	}
 
-    public function free_donations() {
-        $years = array();
-        $start = 2015;
-        $end = date('Y');
-        while ( $start <= $end ){
-            $years[] = $start;
-            $start++;
-        }
+	public function free_donations() {
+		$years = array();
+		$start = 2015;
+		$end = date('Y');
+		while ($start <= $end) {
+			$years[] = $start;
+			$start++;
+		}
 
-        $month = null;
-        $year = null;
-        if(isset($_GET['mes']) && $_GET['mes'] != 0)
-            $month = $_GET['mes'];
-        if(isset($_GET['ano']) && $_GET['ano'] != 0)
-            $year = $_GET['ano'];
+		$month = null;
+		$year = null;
+		if (isset($_GET['mes']) && $_GET['mes'] != 0)
+			$month = $_GET['mes'];
+		if (isset($_GET['ano']) && $_GET['ano'] != 0)
+			$year = $_GET['ano'];
 
-        $data['year'] = $year;
-        $data['years'] = $years;
-        $data['month'] = $month;
-        $data['donations'] = $this->donation_model->getDonationsDetailed(DONATION_TYPE_FREEDONATION, $month, $year);
-        $this->loadReportView("reports/finances/donations", $data);
-    }
-	
+		$data['year'] = $year;
+		$data['years'] = $years;
+		$data['month'] = $month;
+		$data['donations'] = $this -> donation_model -> getDonationsDetailed(DONATION_TYPE_FREEDONATION, $month, $year);
+		$this -> loadReportView("reports/finances/donations", $data);
+	}
 
 }
--- application/views/reports/users/all_users.php
+++ application/views/reports/users/all_users.php
@@ -3,72 +3,139 @@
         <meta charset="UTF-8">
         <title>Colônia Kinderland</title>
 
-        <link href="<?= $this->config->item('assets'); ?>css/basic.css" rel="stylesheet" />
-        <link href="<?= $this->config->item('assets'); ?>css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/themes/base/jquery-ui.css" />
-        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/bootstrap-switch.min.css">
-        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/theme.default.css" />
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery-2.0.3.min.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/ui/jquery-ui.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquerysettings.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.redirect.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/formValidationFunctions.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap-switch.min.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.mask.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery.tablesorter.js"></script>
-        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>datatable/js/datatable.min.js"></script>
-        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>datatable/css/datatable-bootstrap.min.css" />
+        <link href="<?= $this -> config -> item('assets'); ?>css/basic.css" rel="stylesheet" />
+        <link href="<?= $this -> config -> item('assets'); ?>css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="<?= $this -> config -> item('assets'); ?>css/themes/base/jquery-ui.css" />
+        <link rel="stylesheet" href="<?= $this -> config -> item('assets'); ?>css/bootstrap-switch.min.css">
+        <link rel="stylesheet" href="<?= $this -> config -> item('assets'); ?>css/theme.default.css" />
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/ui/jquery-ui.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/jquerysettings.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/jquery/jquery.redirect.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/formValidationFunctions.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/bootstrap-switch.min.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/jquery/jquery.mask.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>js/jquery.tablesorter.js"></script>
+        <script type="text/javascript" src="<?= $this -> config -> item('assets'); ?>datatable/js/datatable.min.js"></script>
+        <link rel="stylesheet" href="<?= $this -> config -> item('assets'); ?>datatable/css/datatable-bootstrap.min.css" />
 		
 
     </head>
     <body>
         <script>
-			$( document ).ready(function() {
-				$('#sortable-table').datatable({
+    		function post(path, params, method) {
+    			method = method || "post"; // Set method to post by default if not specified.
+
+		    	// The rest of this code assumes you are not using a library.
+    			// It can be made less wordy if you use one.
+			    var form = document.createElement("form");
+			    form.setAttribute("method", method);
+			    form.setAttribute("action", path);
+
+			    for(var key in params) {
+			        if(params.hasOwnProperty(key)) {
+			            var hiddenField = document.createElement("input");
+			            hiddenField.setAttribute("type", "hidden");
+			            hiddenField.setAttribute("name", key);
+			            hiddenField.setAttribute("value", params[key]);
+			            form.appendChild(hiddenField);
+			         }
+			    }
+			
+			    document.body.appendChild(form);
+			    form.submit();
+			}
+
+			function getCSVName(){
+				var filtros = $(".datatable-filter");
+				var filtroNome = filtros[0].value;
+				var filtroEmail = filtros[1].value;
+				var nomePadrao = "cadastro-kinderland";
+				if(filtroNome == "" && filtroEmail == ""){
+					var filtroSocio = filtros[2].value;
+					if(filtroSocio == "-1")
+						return nomePadrao.concat("-completo");
+					else {
+						var addToName = "-".concat(filtroSocio.concat("s")); //Pega identificador do filtro e adiciona - e um s no final.
+						return nomePadrao.concat(addToName);						
+					}	
+				}
+				else{
+					return "cadastro-kinderland-filtrado";
+				}
+			}			
+			
+
+	        function sendTableToCSV(){
+	    		var data = [];
+	    		var table = document.getElementById("tablebody");
+	    		var name = getCSVName();
+	    		var tablehead = document.getElementsByTagName("thead")[0];
+	    		for (var i = 0, row; row = table.rows[i]; i++) {
+	    			var data2 = []
+	            	//Nome, retira pega o que esta entre um <> e outro <>
+					data2.push(row.cells[0].innerHTML.split("<")[1].split(">")[1]);
+	            	data2.push(row.cells[1].innerHTML);
+	            	data.push(data2)
+		        } 
+		        if(i==0){
+       				alert('Não há dados para geração da planilha');
+       				return;
+		        }
+		        var dataToSend = JSON.stringify(data);
+		        var columName = ["Nome","Email"]
+		        var columnNameToSend = JSON.stringify(columName);
+		        
+		        post('<?= $this -> config -> item('url_link'); ?>reports/toCSV', {data: dataToSend,name: name,columName: columnNameToSend});
+			}
+
+				$( document ).ready(function() {
+					$('#sortable-table').datatable({
 					pageSize:         Number.MAX_VALUE,
-    				sort: [
-    				function (l, r) { return l.toLowerCase().localeCompare(r.toLowerCase()); }, //Evita problemas com caps-lock
-    				function (l, r) { return l.toLowerCase().localeCompare(r.toLowerCase()); }, //Evita problemas com caps-lock
-    				true],
-    				filters: [true, true, {
-    /* The jQuery element to use (if you want to use a custom select element), if not specified, a new select
-    will be created. */
-    element: null,
-    /* The list of options. Notice that {0: "Option 0"} will output <option value=0>Option 0</option>, so the value used to filter is 0, not "Option 0". 
-    HINT: If you don't want to specify the values manually, you can set this values: "auto" which will retrieve values from the table. */
-    values: {"-1": "Todos", "benemerito": "Sócios benemeritos", "contribuinte": "Sócios contribuintes", "não sócio": "Não associados"}, 
-    /* The select default selected options, can be either a value or an array (for multiple select). If not specified, no value will be selected (for simple select), or all the values (for multiple select). */
-	default: -1,
-    /* Specify if an empty entry should be added. Default value is true. This parameter as no effect on multiple select. */
-    empty: false,
-    /* True for multiple select, false for simple. Default value is false. */
-    multiple: true,
-    /* Will not create a column for this filter. Useful if you want to filter data on a field which is not displayed in the table. */
-    noColumn: false,
-    /* A custom filter function to specify if you don't want the value to be filtered according to the options key. */
-    fn: function (data, selected) {
-        /* Note that selected is always an array of string. */
-        if (selected.length > 0) {
-        	var i = 0;
-        	for(i=0;i<selected.length;i++)
-        		if(selected[i] === data || selected[i] === "-1")
-        			return true;
-    		return false;
-        }
-        /* Note that when using multiple select, selected will contain selected keys.
-        When the empty value is picked, the selected array will contain all available keys. */
-        return true ;
-    }
-}],
-    				filterText: 'Escreva para filtrar... '
-				});
-			});
+					sort: [
+					function (l, r) { return l.toLowerCase().localeCompare(r.toLowerCase()); }, //Evita problemas com caps-lock
+					function (l, r) { return l.toLowerCase().localeCompare(r.toLowerCase()); }, //Evita problemas com caps-lock
+					true],
+					filters: [true, true, {
+					/* The jQuery element to use (if you want to use a custom select element), if not specified, a new select
+					will be created. */
+					element: null,
+					/* The list of options. Notice that {0: "Option 0"} will output <option value=0>Option 0</option>, so the value used to filter is 0, not "Option 0".
+					HINT: If you don't want to specify the values manually, you can set this values: "auto" which will retrieve values from the table. */
+					values: {"-1": "Todos", "todos-socio": "Todos os sócios", "benemerito": "Sócios benemeritos", "contribuinte": "Sócios contribuintes", "não sócio": "Não associados"},
+					/* The select default selected options, can be either a value or an array (for multiple select). If not specified, no value will be selected (for simple select), or all the values (for multiple select). */
+					default: -1,
+					/* Specify if an empty entry should be added. Default value is true. This parameter as no effect on multiple select. */
+					empty: false,
+					/* True for multiple select, false for simple. Default value is false. */
+					multiple: false,
+					/* Will not create a column for this filter. Useful if you want to filter data on a field which is not displayed in the table. */
+					noColumn: false,
+					/* A custom filter function to specify if you don't want the value to be filtered according to the options key. */
+					fn: function (data, selected) {
+					/* Note that selected is always an array of string. */
+					if (selected.length > 0) {
+						var i = 0;
+						if(selected[0] === data || selected[0] === "-1")
+							return true;
+						else if(selected[0] === "todos-socio" && (data==="benemerito" || data==="contribuinte"))
+							return true;
+						return false;
+					}
+					/* Note that when using multiple select, selected will contain selected keys.
+					When the empty value is picked, the selected array will contain all available keys. */
+					return true ;
+					}
+					}],
+					filterText: 'Escreva para filtrar... '
+					});
+					});
         </script>
         <div class="main-container-report">
             <div class = "row">
                 <div class="col-lg-12">
-
+					<button class="button" onclick="sendTableToCSV()" value="">Fazer download da tabela abaixo como csv</button>
                     <table class="table table-bordered table-striped table-min-td-size" style="max-width: 600px; font-size:15px" id="sortable-table">
                         <thead>
                             <tr>
@@ -77,17 +144,17 @@
                                 <th> Sócio </th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody id="tablebody">
                             <?php
                             foreach ($users as $user) {
                                 ?>
                                 <tr>
-                                    <td><a id="<?= $user->fullname ?>" target="_blank" href="<?= $this->config->item('url_link') ?>user/details?id=<?= $user->person_id ?>"><?= $user->fullname ?></a></td>
-                                    <td><?= $user->email ?></td>
-                                    <td><?= $user->associate ?></td>
+                                    <td><a id="<?= $user -> fullname ?>" target="_blank" href="<?= $this -> config -> item('url_link') ?>user/details?id=<?= $user -> person_id ?>"><?= $user -> fullname ?></a></td>
+                                    <td><?= $user -> email ?></td>
+                                    <td><?= $user -> associate ?></td>
                                 </tr>
                                 <?php
-                            }
+								}
                             ?>
                         </tbody>
                     </table>
