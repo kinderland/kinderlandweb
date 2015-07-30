b7e6107a9e1b83cdb195145fdd684dfcb7000bf1
Patrick Sava <patrick.sava@gmail.com>
2015-07-30 20:16:05 -0300

    Indicando inatividade nas em opções do menu de usuário

--- application/views/include/common_user_left_menu.php
+++ application/views/include/common_user_left_menu.php
@@ -2,8 +2,8 @@
     <ul class="nav nav-pills nav-stacked">
         <li><a href="<?= $this->config->item('url_link'); ?>user/edit">Cadastro</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>donations/freeDonation">Doação Avulsa</a></li>
-        <li><a>Campanha de sócios<br />(2015 encerrada)</a></li>
-        <li><a>Inscrições Eventos</a></li>
+        <li><a>Campanha de sócios<br />(inativo)</a></li>
+        <li><a>Inscrições Eventos<br />(inativo)</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>summercamps/index">Inscrições Colonia</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>payments/history">Histórico de doações</a></li>
     </ul>
