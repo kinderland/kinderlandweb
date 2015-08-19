bc574144b168c4ef107cd99bb1ada408257a5880
cadu <cadurosar@gmail.com>
2015-08-19 15:00:15 +0200

    bug 916 corrigido. Estava aceitando tudo

--- application/models/summercamp_model.php
+++ application/models/summercamp_model.php
@@ -323,7 +323,9 @@ class summercamp_model extends CK_Model {
 
         $splitByDot = explode(".", $fileName);
         $extension = $splitByDot[count($splitByDot) - 1];
-        if (!strcasecmp("jpg",$extension) && !strcasecmp("jpeg",$extension) && !strcasecmp("png",$extension) && !strcasecmp("pdf",$extension))
+        if ( !
+        	(strcasecmp("jpg",$extension) == 0 || strcasecmp("jpeg",$extension) == 0 || strcasecmp("png",$extension) == 0 || strcasecmp("pdf",$extension) == 0)
+		   )
             return FALSE;
         $sql = 'INSERT INTO document (summer_camp_id,colonist_id,user_id,filename,extension,document_type,file) VALUES (?, ?, ?, ?,?,?,?)';
         $returnId = $this->execute($this->db, $sql, array($summerCampId, $colonistId, $userId, $fileName, $extension, $type, pg_escape_bytea($file)));
