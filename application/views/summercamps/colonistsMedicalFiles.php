e6070e392c661eabc86da6fe0e08e3cb173a3136
Patrick Sava <patrick.sava@gmail.com>
2015-09-30 18:29:18 -0300

    Correção em função js do sistema de fichas médicas

--- application/views/summercamps/colonistsMedicalFiles.php
+++ application/views/summercamps/colonistsMedicalFiles.php
@@ -45,31 +45,8 @@
             return a-b;
         }
 
-        function post(path, params, method) {
-            method = method || "post"; // Set method to post by default if not specified.
-
-            // The rest of this code assumes you are not using a library.
-            // It can be made less wordy if you use one.
-            var form = document.createElement("form");
-            form.setAttribute("method", method);
-            form.setAttribute("action", path);
-
-            for (var key in params) {
-                if (params.hasOwnProperty(key)) {
-                    var hiddenField = document.createElement("input");
-                    hiddenField.setAttribute("type", "hidden");
-                    hiddenField.setAttribute("name", key);
-                    hiddenField.setAttribute("value", params[key]);
-                    form.appendChild(hiddenField);
-                }
-            }
-
-            document.body.appendChild(form);
-            form.submit();
-        }
-        
         function createPDFMedicalFiles() {
-            window.location.href = "<?= $this->config->item('url_link'); ?>summercamps/generatePDFWithColonistMedicalFiles/<?=$ano_escolhido?>/<?=$summer_camp_id?>/<?=$pavilhao?>/<?=$quarto?>";
+            window.location.href = "<?= $this->config->item('url_link'); ?>summercamps/generatePDFWithColonistMedicalFiles/<?=$ano_escolhido?>/<?=(isset($summer_camp_id)?$summer_camp_id:'')?>/<?=(isset($pavilhao)?$pavilhao:'')?>/<?=(isset($quarto)?$quarto:'')?>";
         }
 
 
