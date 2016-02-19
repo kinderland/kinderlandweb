d4832849261e978f9eb344a43f32a23e048b80f7
Guido Dowsley Paes Leme <dowsleyguido@gmail.com>
2016-02-19 14:47:43 -0200

    Ajeitando o texto da Campanha de sócios - tela do usuario comum

--- application/views/campaign/index.php
+++ application/views/campaign/index.php
@@ -41,16 +41,16 @@ div.scroll{
                     <hr class="footer-hr"/>
              <div class="row">
             <div class="col-lg-9">
-                <p align="justify">
+                <p align="left">
                 <?php if (isset($associate) && !empty($associate) || (isset($benemerito) && $benemerito)) { ?>
                 	<div class="scroll">
-                      <h3 align="justify"><?php echo $fullname ?>, você já é <?= ($gender == "M") ? "sócio" : "sócia" ?>. Obrigado por sua contribuição.</h3>
+                      <h3 align="left"><?php echo $fullname ?>, você já é <?= ($gender == "M") ? "sócio" : "sócia" ?>. Obrigado por sua contribuição.</h3>
                       </div>
                       <?php }?>
-                <p align="justify"> A Associação KINDERLAND é uma entidade sem fins lucrativos que necessita de contribuições e doações regulares. Elas são utilizadas na manutenção e investimentos no espaço onde a Colônia de Férias é realizada, além de ajudar com os demais custos institucionais.</p>
-                <p align="justify"> Estas doações podem ser espontâneas e feitas a qualquer momento, como contribuições de ítens úteis para a colônia de férias, material de construção ou equipamentos em geral. Agradecemos a todos que indistintamente contribuem regularmente como associados ou doadores avulsos.</p>
-                <p align="justify"> A Associação Kinderland realiza projetos sociais com jovens de comunidades carentes, oferece bolsas parciais ou integrais para colonistas nas temporadas de verão e participa de várias outras iniciativas comunitárias. Somente com estas doações tudo isto torna-se possível.</p>
-                <p align="justify"> Todos os anos realizamos uma campanha de associados Kinderland, visando obter recursos financeiros para as nossos projetos, investimentos e operações ao longo do ano. Os associados da Kinderland têm, entre outros, os seguintes benefícios:</p>
+                <p align="left"> A Associação KINDERLAND é uma entidade sem fins lucrativos que necessita de contribuições e doações regulares. Elas são utilizadas na manutenção e investimentos no espaço onde a Colônia de Férias é realizada, além de ajudar com os demais custos institucionais.</p>
+                <p align="left"> Estas doações podem ser espontâneas e feitas a qualquer momento, como contribuições de ítens úteis para a colônia de férias, material de construção ou equipamentos em geral. Agradecemos a todos que indistintamente contribuem regularmente como associados ou doadores avulsos.</p>
+                <p align="left"> A Associação Kinderland realiza projetos sociais com jovens de comunidades carentes, oferece bolsas parciais ou integrais para colonistas nas temporadas de verão e participa de várias outras iniciativas comunitárias. Somente com estas doações tudo isto torna-se possível.</p>
+                <p align="left"> Todos os anos realizamos uma campanha de associados Kinderland, visando obter recursos financeiros para as nossos projetos, investimentos e operações ao longo do ano. Os associados da Kinderland têm, entre outros, os seguintes benefícios:</p>
                 <ul>
                     <li>
                         Desconto nas doações pela cessão do espaço físico da colônia Kinderland para festas de aniversário, finais de semana com amigos ou outros eventos particulares;
@@ -63,19 +63,19 @@ div.scroll{
                     </li>
                 </ul>
 
-                <p align="justify">
+                <p align="left">
                     A Associação Kinderland considera como associado as pessoas pertencentes ao nosso cadastro que contribuírem com um valor igual a
                      <b>R$ <?php echo $campaign->getPrice(); ?> a partir do dia <?php echo $date_start ?> até o dia <?php echo $date_finish ?>.</b>
                     
 
                 </p>
-                <p align="justify">
+                <p align="left">
                     O valor da colaboração do associado pode ser doado em pagamento por cartão de crédito, em uma ou até 3 (três) parcelas iguais sem acréscimo, em função da quantidade de meses restantes até o final do mês de junho. Também há a opção de doação por cartão de débito. <i>Por questões de segurança e controle administrativo, não é possível receber doações por outros meios (transferência bancária, DOC), cheque ou dinheiro</i>.
                 </p>
-                <p align="justify">
+                <p align="left">
                     Se você <span style="font-weight:bold"> concorda com as condições </span>para ser um associado Kinderland, basta confirmar abaixo em “Quero ser Sócio” e escolher como realizar sua doação. Ao final do processo, e com a devida autorização da administradora do seu cartão, um email será enviado automaticamente, confirmando a condição de associado. A doação também aparecerá em seu “Histórico de Doações” uma vez confirmada.
                 </p>
-                <p align="justify">
+                <p align="left">
                     Caso não receba a mensagem de confirmação, por alguma demora na autorização por parte da Cielo ou da administradora de cartões, pedimos para entrar em contato conosco por telefone (21 2266-1980) ou e-mail (secretaria@kinderland.com.br) antes de efetuar nova tentativa de doação.
                 </p>
  					<?php if (isset($associate) && !empty($associate)) { ?>
@@ -92,15 +92,15 @@ div.scroll{
              <div class="row">
             <div class="col-lg-9">
                 <p align="justify">
-                    	<?php if (isset($associate) || !empty($associate) || (isset($benemerito) && $benemerito)) { ?>
+                    	<?php if (!empty($associate) || (isset($benemerito) && $benemerito)) { ?>
                    <div class="scroll">
-                      <h3 align="justify"><?php echo $fullname ?>, você já é <?= ($gender == "M") ? "sócio" : "sócia" ?>. Obrigado por sua contribuição.</h3>
-                      <h3 align="justify">Campanha encerrada em <?php echo $date_finish ?>. </h3>
+                      <h3 align="left"><?php echo $fullname ?>, você já é <?= ($gender == "M") ? "sócio" : "sócia" ?>. Obrigado por sua contribuição.</h3>
+                      <h3 align="left">Campanha encerrada em <?php echo $date_finish ?>. </h3>
                    </div>
-                <p align="justify"> A Associação KINDERLAND é uma entidade sem fins lucrativos que necessita de contribuições e doações regulares. Elas são utilizadas na manutenção e investimentos no espaço onde a Colônia de Férias é realizada, além de ajudar com os demais custos institucionais.</p>
-                <p align="justify"> Estas doações podem ser espontâneas e feitas a qualquer momento, como contribuições de ítens úteis para a colônia de férias, material de construção ou equipamentos em geral. Agradecemos a todos que indistintamente contribuem regularmente como associados ou doadores avulsos.</p>
-                <p align="justify"> A Associação Kinderland realiza projetos sociais com jovens de comunidades carentes, oferece bolsas parciais ou integrais para colonistas nas temporadas de verão e participa de várias outras iniciativas comunitárias. Somente com estas doações tudo isto torna-se possível.</p>
-                <p align="justify"> Todos os anos realizamos uma campanha de associados Kinderland, visando obter recursos financeiros para as nossos projetos, investimentos e operações ao longo do ano. Os associados da Kinderland têm, entre outros, os seguintes benefícios:</p>
+                <p align="left"> A Associação KINDERLAND é uma entidade sem fins lucrativos que necessita de contribuições e doações regulares. Elas são utilizadas na manutenção e investimentos no espaço onde a Colônia de Férias é realizada, além de ajudar com os demais custos institucionais.</p>
+                <p align="left"> Estas doações podem ser espontâneas e feitas a qualquer momento, como contribuições de ítens úteis para a colônia de férias, material de construção ou equipamentos em geral. Agradecemos a todos que indistintamente contribuem regularmente como associados ou doadores avulsos.</p>
+                <p align="left"> A Associação Kinderland realiza projetos sociais com jovens de comunidades carentes, oferece bolsas parciais ou integrais para colonistas nas temporadas de verão e participa de várias outras iniciativas comunitárias. Somente com estas doações tudo isto torna-se possível.</p>
+                <p align="left"> Todos os anos realizamos uma campanha de associados Kinderland, visando obter recursos financeiros para as nossos projetos, investimentos e operações ao longo do ano. Os associados da Kinderland têm, entre outros, os seguintes benefícios:</p>
                 <ul>
                     <li>
                         Desconto nas doações pela cessão do espaço físico da colônia Kinderland para festas de aniversário, finais de semana com amigos ou outros eventos particulares;
@@ -113,39 +113,39 @@ div.scroll{
                     </li>
                 </ul>
 
-                <p align="justify">
+                <p align="left">
                     A Associação Kinderland considera como associado as pessoas pertencentes ao nosso cadastro que contribuírem com um valor igual a 
                     <b>R$ <?php echo $campaign->getPrice(); ?> a partir do dia <?php echo $date_start ?> até o dia <?php echo $date_finish ?>.</b>
                     
 
                 </p>
-                <p align="justify">
+                <p align="left">
                     O valor da colaboração do associado pode ser doado em pagamento por cartão de crédito, em uma ou até 3 (três) parcelas iguais sem acréscimo, em função da quantidade de meses restantes até o final do mês de junho. Também há a opção de doação por cartão de débito. <i>Por questões de segurança e controle administrativo, não é possível receber doações por outros meios (transferência bancária, DOC), cheque ou dinheiro</i>.
                 </p>
-                <p align="justify">
+                <p align="left">
                     Se você <span style="font-weight:bold"> concorda com as condições </span>para ser um associado Kinderland, basta confirmar abaixo em “Quero ser Sócio” e escolher como realizar sua doação. Ao final do processo, e com a devida autorização da administradora do seu cartão, um email será enviado automaticamente, confirmando a condição de associado. A doação também aparecerá em seu “Histórico de Doações” uma vez confirmada.
                 </p>
-                <p align="justify">
+                <p align="left">
                     Caso não receba a mensagem de confirmação, por alguma demora na autorização por parte da Cielo ou da administradora de cartões, pedimos para entrar em contato conosco por telefone (21 2266-1980) ou e-mail (secretaria@kinderland.com.br) antes de efetuar nova tentativa de doação.
                 </p>
 				                
 				                
                       	<?php } else {?>
-               		<p align="justify">
+               		<p align="left">
                 	<strong> A campanha de sócios <?php echo $year ?>  já foi encerrada.</strong>
                 	</p>
                     </p>
                 <?php } ?>
                    <?php  } else {
                     ?>
-                    <p align="justify">
+                    <p align="left">
                     <strong> Aguarde, em breve iniciaremos a campanha de sócios <?php echo $year ?> .</strong>
                     </p>
                     
                     
                     
                 <?php }} else{?>
-                    <p align="justify">
+                    <p align="left">
                     <strong> Aguarde, em breve iniciaremos a campanha de sócios <?php echo $year ?> .</strong>
                     </p>
                 <?php }?>
