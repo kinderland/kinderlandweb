0b90d96f8d5e4848b475fade8ee03e658f242571
Brenno Tavares de Carvalho <brennotc@gmail.com>
2016-02-25 12:19:19 -0300

    Alterações em Eventos

--- application/views/include/common_user_left_menu.php
+++ application/views/include/common_user_left_menu.php
@@ -6,6 +6,6 @@
         <li><a href="<?= $this->config->item('url_link'); ?>events/index">Inscrições Eventos</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>summercamps/index">Inscrições Colonia</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>payments/history">Histórico de doações</a></li>
-        <li><a href="<?= $this->config->item('url_link'); ?>user/emails">Histórico de <br>e-mails</a></li>
+      <!--  <li><a href="<?php // echo $this->config->item('url_link'); ?>user/emails">Histórico de <br>e-mails</a></li> -->
     </ul>
 </div>
