4e5cea29f9392d3967033557dd73664382debed1
Matheus Henrique Prates <mhprates96@gmail.com>
2016-03-17 14:32:30 -0300

    Começando o codigo da exportação de tabela como csv. Ainda não pronto.

--- application/views/reports/finances/donation_panel.php
+++ application/views/reports/finances/donation_panel.php
@@ -13,152 +13,203 @@
         <title>Colônia Kinderland</title>
 
         <link href="<?= $this->config->item('assets'); ?>css/basic.css" rel="stylesheet" />
-        <!--<link href="<?= $this->config->item('assets'); ?>css/old/screen.css" rel="stylesheet" />-->
         <link href="<?= $this->config->item('assets'); ?>css/bootstrap.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/themes/base/jquery-ui.css" />
-        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/bootstrap-switch.min.css"></script>
-    <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/theme.default.css" />
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery-2.0.3.min.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/ui/jquery-ui.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquerysettings.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.redirect.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/formValidationFunctions.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap-switch.min.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.mask.js"></script>
-    <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery.tablesorter.js"></script>
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/bootstrap-switch.min.css">
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>css/theme.default.css" />
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/ui/jquery-ui.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquerysettings.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.redirect.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/formValidationFunctions.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/bootstrap-switch.min.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery/jquery.mask.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>js/jquery.tablesorter.js"></script>
+        <script type="text/javascript" src="<?= $this->config->item('assets'); ?>datatable/js/datatable.min.js"></script>
+        <link rel="stylesheet" href="<?= $this->config->item('assets'); ?>datatable/css/datatable-bootstrap.min.css" />
 
-</head>
-<style>
 
-    div.pad{
-        padding-left:20%;
+    </head>
+    <style>
+
+        div.pad{
+            padding-left:20%;
+        }
+
+    </style>
+
+    <?php
+
+    function formatarEMostrar($valor) {
+
+        echo number_format($valor, 0, ",", ".");
     }
 
-</style>
+    function getMonthName($m) {
+        switch ($m) {
+            case 1: return "Janeiro";
+            case 2: return "Fevereiro";
+            case 3: return "Março";
+            case 4: return "Abril";
+            case 5: return "Maio";
+            case 6: return "Junho";
+            case 7: return "Julho";
+            case 8: return "Agosto";
+            case 9: return "Setembro";
+            case 10: return "Outubro";
+            case 11: return "Novembro";
+            case 12: return "Dezembro";
+        }
+    }
 
-<script>
+    function month_and_year($year, $month) {
+        $name = getMonthName($month);
+        echo $name . " / " . $year;
+    }
+    
+    $firstDate = getMonthName($selected_months[0]) . $selected_years[0];
+    $i = count($selected_years) - 1;
+    $lastDate = getMonthName($selected_months[$i]) . $selected_years[$i];
+    $date=date("Y-m-d H:i:s");
+    ?>
+
+    <body>
+        <script>
 
 
 <?php if ($error && !empty($error)) { ?>
-        alert("<?php echo $error; ?>");
+                alert("<?php echo $error; ?>");
 <?php }
 ?>
 
-</script>
-<?php
+            function getCSVName() {
+                
+                var nomePadrao = " receitas-";
+                nomePadrao=nomePadrao.concat("<?php echo $firstDate ?>-");
+                nomePadrao=nomePadrao.concat("<?php echo $lastDate ?>-");
+                nomePadrao=nomePadrao.concat("<?php echo $date ?>");
+                return nomePadrao;
+            }
 
-function formatarEMostrar($valor) {
 
-    echo number_format($valor, 0, ",", ".");
-}
+            function sendTableToCSV() {
+                var data = [];
+                var table = document.getElementById("tablebody");
+                var name = getCSVName();
+                var tablehead = document.getElementsByTagName("thead")[0];
+                //alert(tablehead.rows[0].cells[1].innerHTML);
+                //Isso aqui chega até a informação que eu quero.
+                for (var i = 0, row; row = table.rows[i]; i++) {
+                    var data2 = [];
+                    //Nome, retira pega o que esta entre um <> e outro <>
+                    data2.push(row.cells[0].innerHTML.split("<")[1].split(">")[1]);
+                    data2.push(row.cells[1].innerHTML);
+                    data.push(data2);
+                }
+                if (i == 0) {
+                    alert('Não há dados para geração da planilha');
+                    return;
+                }
+                var dataToSend = JSON.stringify(data);
+                var columName = ["Receita", "Valor", "Mes", "Ano"]
+                var columnNameToSend = JSON.stringify(columName);
 
-function getMonthName($m) {
-    switch ($m) {
-        case 1: return "Janeiro";
-        case 2: return "Fevereiro";
-        case 3: return "Março";
-        case 4: return "Abril";
-        case 5: return "Maio";
-        case 6: return "Junho";
-        case 7: return "Julho";
-        case 8: return "Agosto";
-        case 9: return "Setembro";
-        case 10: return "Outubro";
-        case 11: return "Novembro";
-        case 12: return "Dezembro";
-    }
-}
+                post('<?= $this->config->item('url_link'); ?>reports/toCSV', {data: dataToSend, name: name, columName: columnNameToSend});
+            }
+        </script>
+        <div class = "row">
 
-function month_and_year($year, $month) {
-    $name = getMonthName($month);
-    echo $name . " / " . $year;
-}
-?>
+            <div class="col-lg-10" bgcolor="red">
+                <button class="button" onclick="sendTableToCSV()" value="">Fazer download da tabela abaixo como csv</button>
+                <form method="GET">
+                    <div style="padding-left:3px">
+                        <p> De: 
+                            Ano: <select name="year_start" onchange="this.form.submit()" id="year_start">
+                                <?php
+                                foreach ($years as $y) {
+                                    $selected = "";
+                                    if ($y == $year_start)
+                                        $selected = "selected";
+                                    echo "<option $selected value='$y'>$y</option>";
+                                }
+                                ?>
+                            </select> 
 
-<body>
-    <div class = "row">
-
-        <div class="col-lg-10" bgcolor="red">
-
-            <form method="GET">
-                <div style="padding-left:3px">
-                    <p> De: 
-                        Ano: <select name="year_start" onchange="this.form.submit()" id="year_start">
+                            Mês: <select name="month_start"  onchange="this.form.submit()" id="month_start">
+                                <?php
+                                for ($m = 1; $m <= 12; $m++) {
+                                    $selected = "";
+                                    if ($m == $month_start)
+                                        $selected = "selected";
+                                    echo "<option $selected value='$m'>" . getMonthName($m) . "</option>";
+                                }
+                                ?>
+                            </select> </p>
+                    </div>
+                    <br/> 
+                    <p>                Até:  Ano: <select name="year_finish" onchange="this.form.submit()" id="year_finish">
                             <?php
                             foreach ($years as $y) {
                                 $selected = "";
-                                if ($y == $year_start)
+                                if ($y == $year_finish)
                                     $selected = "selected";
                                 echo "<option $selected value='$y'>$y</option>";
                             }
                             ?>
-                        </select> 
-
-                        Mês: <select name="month_start"  onchange="this.form.submit()" id="month_start">
+                        </select>
+                        Mês: <select name="month_finish" onchange="this.form.submit()" id="month_finish">
                             <?php
                             for ($m = 1; $m <= 12; $m++) {
                                 $selected = "";
-                                if ($m == $month_start)
+                                if ($m == $month_finish)
                                     $selected = "selected";
                                 echo "<option $selected value='$m'>" . getMonthName($m) . "</option>";
                             }
                             ?>
-                        </select> </p>
-                </div>
-                <br/> 
-                <p>                Até:  Ano: <select name="year_finish" onchange="this.form.submit()" id="year_finish">
-                        <?php
-                        foreach ($years as $y) {
-                            $selected = "";
-                            if ($y == $year_finish)
-                                $selected = "selected";
-                            echo "<option $selected value='$y'>$y</option>";
-                        }
-                        ?>
-                    </select>
-                    Mês: <select name="month_finish" onchange="this.form.submit()" id="month_finish">
-                        <?php
-                        for ($m = 1; $m <= 12; $m++) {
-                            $selected = "";
-                            if ($m == $month_finish)
-                                $selected = "selected";
-                            echo "<option $selected value='$m'>" . getMonthName($m) . "</option>";
-                        }
-                        ?>
-                    </select></p>
-            </form>
+                        </select></p>
+                </form>
 
-            <div class="pad"> 
-                <h4 style="margin-top:30px;"> Total do período: <?php echo formatarEMostrar(intval($total)); ?> </h4>
-                <table class="table table-bordered table-striped table-min-td-size" style="max-width: 800px; font-size:15px;">
-                    <tr>
-                        <td style="text-align: center;"><h4> <b> Período </b></h4> </td>
-                        <td style="text-align: center;"><h4> <b> Avulsa</b></h4> </td>
-                        <td style="text-align: center;"><h4> <b> Campanha </b></h4> </td>
-                        <td style="text-align: center;"><h4> <b> Colônia </b></h4> </td>
-                       <!-- <td style="text-align: center;"><h4> <b> MiniKinderland </b></h4> </td> -->
-                        <td style="text-align: center;"><h4> <b> Total </b></h4> </td>
-                    </tr>
-                    <?php for ($i = 0; $i < count($selected_years); $i++) { ?>
+                <div class="pad"> 
+                    <h4 style="margin-top:30px;"> Total do período: <?php echo formatarEMostrar(intval($total)); ?> </h4>
+                    <table class="table table-bordered table-striped table-min-td-size" style="max-width: 800px; font-size:15px;"id="sortable-table" >
+                        <thead>
                         <tr>
-                            <td> <?php month_and_year($selected_years[$i], $selected_months[$i]); ?> </td>
-                            <td> <?php echo formatarEMostrar(intval($free[$i])); ?></td>
-                            <td> <?php echo formatarEMostrar(intval($campaign[$i])); ?></td>
-                            <td> <?php echo formatarEMostrar(intval($summercamp[$i]));; ?></td>
-                            <!--<td> <?php echo formatarEMostrar(intval($mini[$i]));; ?></td>-->
-                            <td> <?php echo formatarEMostrar(intval($total_per_period[$i])); ?></td>
+                            <td style="text-align: center;"><h4> <b> Período </b></h4> </td>
+                            <td style="text-align: center;"><h4> <b> Avulsa</b></h4> </td>
+                            <td style="text-align: center;"><h4> <b> Campanha </b></h4> </td>
+                            <td style="text-align: center;"><h4> <b> Colônia </b></h4> </td>
+                           <!-- <td style="text-align: center;"><h4> <b> MiniKinderland </b></h4> </td> -->
+                            <td style="text-align: center;"><h4> <b> Total </b></h4> </td>
                         </tr>
-                    <?php } ?>
-                    <tr>
-                        <td> Total </td>
-                        <td> <?php echo formatarEMostrar(intval($total_free)); ?></td>
-                        <td> <?php echo formatarEMostrar(intval($total_campaign)); ?></td>
-                        <td> <?php echo formatarEMostrar(intval($total_summercamp)); ?></td>
-                       <!-- <td> <?php echo formatarEMostrar(intval($total_mini)); ?></td> -->
-                        <td> <?php echo formatarEMostrar(intval($total)); ?></td>
-                    </tr>
+                        </thead>
+                        <tbody id="tablebody">
+                            <?php for ($i = 0; $i < count($selected_years); $i++) { ?>
+                                <tr>
+                                    <td> <?php month_and_year($selected_years[$i], $selected_months[$i]); ?> </td>
+                                    <td> <?php echo formatarEMostrar(intval($free[$i])); ?></td>
+                                    <td> <?php echo formatarEMostrar(intval($campaign[$i])); ?></td>
+                                    <td> <?php
+                                        echo formatarEMostrar(intval($summercamp[$i]));
+                                        ;
+                                        ?></td>
+                                    <!--<td> <?php
+                                    echo formatarEMostrar(intval($mini[$i]));
+                                    ;
+                                    ?></td>-->
+                                    <td> <?php echo formatarEMostrar(intval($total_per_period[$i])); ?></td>
+                                </tr>
+                            <?php } ?>
+                            <tr>
+                                <td> Total </td>
+                                <td> <?php echo formatarEMostrar(intval($total_free)); ?></td>
+                                <td> <?php echo formatarEMostrar(intval($total_campaign)); ?></td>
+                                <td> <?php echo formatarEMostrar(intval($total_summercamp)); ?></td>
+                               <!-- <td> <?php echo formatarEMostrar(intval($total_mini)); ?></td> -->
+                                <td> <?php echo formatarEMostrar(intval($total)); ?></td>
+                        </tbody>
+                        </tr>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
