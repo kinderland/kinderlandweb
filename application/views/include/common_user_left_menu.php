1e0f4a1b541618f74b7085425f4f34552b45473f
Brenno Tavares de Carvalho <brennotc@gmail.com>
2016-08-19 17:23:21 -0300

    Pequenas alterações no menu lateral

--- application/views/include/common_user_left_menu.php
+++ application/views/include/common_user_left_menu.php
@@ -6,8 +6,8 @@
         <?php if($this -> personuser_model -> isAssociateAndNotTemporary($this->session->userdata("user_id"))){?>
         	<li><a href="<?= $this->config->item('url_link'); ?>admin/createTemporaryAssociate">Indicar CPF</a></li>
         <?php }?>
-        <li><a href="<?= $this->config->item('url_link'); ?>events/index">Inscrições Eventos</a></li>
-        <li><a href="<?= $this->config->item('url_link'); ?>summercamps/indexm">Inscrições Colonia</a></li>
+        <li><a href="<?= $this->config->item('url_link'); ?>events/index">Inscrições MaCK</a></li>
+        <li><a href="<?= $this->config->item('url_link'); ?>summercamps/indexm">Inscrições Colônia</a></li>
         <li><a href="<?= $this->config->item('url_link'); ?>payments/history">Histórico de doações</a></li>
       <li><a href="<?php echo $this->config->item('url_link'); ?>user/emails">Histórico de <br>e-mails</a></li>
     </ul>
