41fb98cfcb250bcbbf3d02bc529ef6d9618f88d7
digitok <ruben@digitok.com.br>
2019-08-11 23:42:59 -0300

    Update viewColonistInfo.php

--- application/views/summercamps/viewColonistInfo.php
+++ application/views/summercamps/viewColonistInfo.php
@@ -1037,11 +1037,10 @@
                 <div class="form-group">
                     <label for="special_care_obs" class="col-lg-6 control-label"> Qual? </label>
                     <div class="col-lg-6">
-                        <textarea disabled Name="specialCareObs" class="col-lg-5" ROWS=5 COLS=20>
-                          <?php 
-if (isset($specialCare))
+                        <textarea disabled Name="specialCareObs" class="col-lg-5" ROWS=5 COLS=20><?php 
+                            if (isset($specialCare))
                               if($specialCare == 't') echo $specialCareObs; ?></textarea>
-	             </div>
+	                   </div>
                 </div>
             </div>
             <br />
