e0aac77af814bd717f39e59feff08920edc32973
digitok <ruben@digitok.com.br>
2020-11-26 15:51:30 -0300

    Update common_user_left_menu.php
    Alteracao no menu para doacao emergencial

--- application/views/include/common_user_left_menu.php
+++ application/views/include/common_user_left_menu.php
@@ -1,7 +1,7 @@
 <div class="col-lg-2  left-action-bar">
     <ul class="nav nav-pills nav-stacked">
         <li><a href="<?= $this->config->item('url_link'); ?>user/edit">Cadastro</a></li>
-        <li><a href="<?= $this->config->item('url_link'); ?>donations/freeDonation">Doação Avulsa</a></li>
+        <li><a href="<?= $this->config->item('url_link'); ?>donations/freeDonation">Doação Emergencial</a></li>
 <!--
         <li><a href="<?= $this->config->item('url_link'); ?>campaigns/index">Campanha de sócios</a></li>
         <?php if($this -> personuser_model -> isAssociateAndNotTemporary($this->session->userdata("user_id"))){?>
