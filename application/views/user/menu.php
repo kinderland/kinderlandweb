8aae37df4fd12100daf51792e3e1f6fac695d91b
digitok <ruben@digitok.com.br>
2020-11-27 09:04:12 -0300

    Update menu.php

--- application/views/user/menu.php
+++ application/views/user/menu.php
@@ -3,4 +3,14 @@
     <div class="col-lg-10" bgcolor="red">
         <h3>Bem <?= ($gender == "M") ? "vindo" : "vinda" ?>, <?= $fullname ?></h3>
     </div>
+    <tr>
+    <p align="justify">Texto doação emergencial...</p>    
+    <tr>
+    <div class="row">
+                    <div  class="col-lg-4 col-lg-offset-4" style="padding-bottom:10px">
+                        <a href="<?= $this->config->item('url_link'); ?>donnation/freedonation">
+                            <button  class="btn btn-primary" style="margin: auto; width: 100%; ">Quero doar</button>
+                        </a>
+                    </div>
+    </div>
 </div>
