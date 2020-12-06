6a6a9fa41b60c44b173a32776b8fbfbbc2141170
digitok <ruben@digitok.com.br>
2020-12-06 20:09:35 -0300

    Update header.php

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -37,7 +37,7 @@
                     <img src="<?= $this->config->item('assets'); ?>images/kinderland/logo-kinderland.png" width=140 height=50 />
                 </a>
                 <a  style="font-size:42px;color:black;justify-content:center;align-items: center">
-                    Voce chegou no lugar certo !
+                    Voce chegou no lugar certo !<br>Entre ou cadastre-se para DOAR para a Kinderland!
                 </a>
                 <div class="navbar-form navbar-right" style="margin-top:20px">
                     <?php if (isset($user_id)) { ?>
