b7ff9d7a4d049ad0d63c18292c21d0538be5137b
digitok <ruben@digitok.com.br>
2020-12-09 20:36:26 -0300

    Update menu.php

--- application/views/user/menu.php
+++ application/views/user/menu.php
@@ -25,10 +25,10 @@
 
 <div class="col-lg-10 middle-content">
         <div class="row">
-            <div class="col-lg-7">
+            <div class="col-lg-8">
                 <img src="<?= $this->config->item('assets'); ?>images/kinderland/KinderlandDoeoeoa.jpeg" height=300/>
 
-        <h4><p>Kinderland,<br>onde passei os melhores momentos, talvez os melhores da vida!</p></h4>
+        <h4><p align="right">Kinderland,<br>onde passei os melhores momentos, talvez os melhores da vida!</p></h4>
 <!--
             <p align="right" style="border: 1px;font-size:16px;color:#428bca;"><button style="border: 3px solid #428bca;color:#428bca;background-color:white;border-radius:5px;pointer-events:none">Valor arrecadado (atualizado às 20:00): R$ 180.123</button></p>a
 -->
