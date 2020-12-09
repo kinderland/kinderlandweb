0ff82e45365315120ebeecf863f202ccce67a2e4
digitok <ruben@digitok.com.br>
2020-12-09 10:32:48 -0300

    Update menu.php

--- application/views/user/menu.php
+++ application/views/user/menu.php
@@ -16,7 +16,8 @@
 </script>
 
 <div class = "row">
-    <?php require_once APPPATH . 'views/include/common_user_left_menu.php' ?>
+    <?php if ($fullname != 'Visitante') require_once APPPATH . 'views/include/common_user_left_menu.php' ?>
+
     <div class="col-lg-10" bgcolor="red">
         <h3>Bem <?= ($gender == "M") ? "vindo" : "vinda" ?>, <?= $fullname ?></h3>
     </div>
