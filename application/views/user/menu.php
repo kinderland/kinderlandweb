cab6aa666cf5d4d3c503f923576b0e11a2b7822d
digitok <ruben@digitok.com.br>
2020-12-10 00:21:23 -0300

    Update menu.php

--- application/views/user/menu.php
+++ application/views/user/menu.php
@@ -133,7 +133,7 @@
         </form>
 
         <br></br>
-        <a href="http://www.kinderland.com.br/colonias-atuais/"><button class="btn btn-secondary" style="font-size:18px;color:#428bca">SOBRE A CAMPANHA</button></a>
+        <a href="http://www.kinderland.com.br/colonias-atuais/"><button class="btn btn-secondary" style="font-size:18px;color:#428bca;border-radius:5px;border:3px solid #428bca">SOBRE A CAMPANHA</button></a>
 
     </div>
 
