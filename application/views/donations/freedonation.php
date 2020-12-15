ee7544d2e1fa86c34a3b5be2b505f6ef4aaab39d
digitok <ruben@digitok.com.br>
2020-12-15 11:37:34 -0300

    valor e percentual

--- application/views/donations/freedonation.php
+++ application/views/donations/freedonation.php
@@ -23,7 +23,7 @@
 <div class="col-lg-10 middle-content">
         <div class="row">
             <div class="col-lg-8">
-                <p align="right" style="border: 1px;font-size:16px;color:#428bca;"><button style="border: 3px solid #428bca;color:black;background-color:white;border-radius:5px;pointer-events:none">Valor arrecadado (atualizado às 21:30): R$ 334.661</button></p>
+                <p align="right" style="border: 1px;font-size:16px;color:#428bca;"><button style="border: 3px solid #428bca;color:black;background-color:white;border-radius:5px;pointer-events:none">Valor arrecadado (atualizado às 11:30): R$ 366.774 (66,68%)</button></p>
             </div>
             <div class="col-lg-8">
                 <img src="<?= $this->config->item('assets'); ?>images/kinderland/KinderlandDoeoeoa.jpeg" align="center" height=360/>
